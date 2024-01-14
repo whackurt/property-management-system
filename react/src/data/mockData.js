e8dd33a5b7088f437d58df03315087cb2ba75acc
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-01-14 17:50:38 +0800

    feat: config

--- react/src/data/mockData.js
+++ react/src/data/mockData.js
@@ -5,7 +5,7 @@ export const mockDataTeam = [
     id: 1,
     name: "Jon Snow",
     email: "jonsnow@gmail.com",
-    age: 35,
+
     phone: "(665)121-5454",
     access: "Admin",
   },
@@ -13,7 +13,7 @@ export const mockDataTeam = [
     id: 2,
     name: "Cersei Lannister",
     email: "cerseilannister@gmail.com",
-    age: 42,
+
     phone: "(421)314-2288",
     access: "Manager",
   },
@@ -21,7 +21,7 @@ export const mockDataTeam = [
     id: 3,
     name: "Jaime Lannister",
     email: "jaimelannister@gmail.com",
-    age: 45,
+
     phone: "(422)982-6739",
     access: "Establishment",
   },
@@ -29,7 +29,7 @@ export const mockDataTeam = [
     id: 4,
     name: "Anya Stark",
     email: "anyastark@gmail.com",
-    age: 16,
+
     phone: "(921)425-6742",
     access: "Admin",
   },
@@ -37,7 +37,7 @@ export const mockDataTeam = [
     id: 5,
     name: "Daenerys Targaryen",
     email: "daenerystargaryen@gmail.com",
-    age: 31,
+
     phone: "(421)445-1189",
     access: "Establishment",
   },
@@ -45,7 +45,7 @@ export const mockDataTeam = [
     id: 6,
     name: "Ever Melisandre",
     email: "evermelisandre@gmail.com",
-    age: 150,
+
     phone: "(232)545-6483",
     access: "Manager",
   },
@@ -53,7 +53,7 @@ export const mockDataTeam = [
     id: 7,
     name: "Ferrara Clifford",
     email: "ferraraclifford@gmail.com",
-    age: 44,
+
     phone: "(543)124-0123",
     access: "Establishment",
   },
@@ -61,7 +61,7 @@ export const mockDataTeam = [
     id: 8,
     name: "Rossini Frances",
     email: "rossinifrances@gmail.com",
-    age: 36,
+
     phone: "(222)444-5555",
     access: "Establishment",
   },
@@ -69,7 +69,7 @@ export const mockDataTeam = [
     id: 9,
     name: "Harvey Roxie",
     email: "harveyroxie@gmail.com",
-    age: 65,
+
     phone: "(444)555-6239",
     access: "Admin",
   },
